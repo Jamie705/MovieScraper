78504d7d2306bb7889124da22e25ac51522a8b84
myusername <rodriguez.jamie@live.com>
2018-12-29 21:06:05 -0500

    Added and removed routes, saving all required fields

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-// var db = require("./models");
+var db = require("./models");
 
 var PORT = 3000;
 
@@ -33,9 +33,7 @@ mongoose.connect("mongodb://localhost/movieScrapper", {
     useNewUrlParser: true
 });
 
-// Save an empty result object
-var results = [];
-// Routes
+// Routes to get articles of movie site
 app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with axios
     axios.get("https://screenrant.com/movie-news/").then(function (response) {
@@ -43,33 +41,98 @@ app.get("/scrape", function (req, res) {
         var $ = cheerio.load(response.data);
                 
         // Now, we grab every h2 within an article tag, and do the following:
-        $("div.bc-info").each(function (i, element) {
+        $("article.half-thumb").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
+//TO DO: If all fields have values, add fields 
             
             // Add the text and href of every link, and save them as properties of the result object
-            var title = $(element)
+            result.title = $(element)
+                .children("div.bc-info")
                 .children("h3")
                 .text();
-            var exerpt = $(element)
+            result.exerpt = $(element)
+                .children("div.bc-info")
                 .children("p")
                 .text();
-            var link = $(element)
+            result.link = $(element)
+                // .children("div.bc-details")
                 .children("a")
                 .attr("href");
 
-//TO DO: add elseIF to only add when all feilds are valid
-            results.push({
-                title: title,
-                exerpt: exerpt,
-                link: link
-            });
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
         });
-
         // Send a message to the client
-        console.log(results);
-    });
- // Send a "Scrape Complete" message to the browser
- res.send("Scrape Complete" + results);
- });
+        res.json(result);
+    });      
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({
+            _id: req.params.id
+        })
+        // ..and populate all of the notes associated with it
+        .populate("note")
+        .then(function (dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+            return db.Article.findOneAndUpdate({
+                _id: req.params.id
+            }, {
+                note: dbNote._id
+            }, {
+                new: true
+            });
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
 // Start the server
 app.listen(PORT, function () {
